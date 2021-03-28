ea45505abe9e7edc9a586f188a4e4668846e7b61
Billy Kirk <kirk.william.c@gmail.com>
2021-03-28 17:48:13 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Lights from "./Lights.svelte";
+  import { lifxClient } from "/home/billy/dev/typescript/lifx/lifx";
+
+  const client = new lifxClient({
+    token: import.meta.env.VITE_LIFX_API_KEY as string,
+  });
+</script>
+
+<Lights lights={client.listLights()} />
---
+++ src/Lights.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let lights;
+</script>
+
+{#await lights then lights}
+    {#each lights as light}
+        <p>{light.label} - {light.power}</p>
+    {/each}
+{/await}
